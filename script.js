e052b28193815793f3927a24efa127a99aedd2da
ibtisam <alexismozez123@gmail.com>
2023-12-02 12:40:42 +0500

    javascript

---
+++ script.js
@@ -0,0 +1,30 @@
+/*
+counter = 0
+while(counter<10){
+    counter++
+    square = counter**2
+
+    if (square<50){
+        console.log(square)
+    }
+    else{
+        break
+    }
+}
+*/
+
+/*dishes = []
+
+while (true){
+    dishname = prompt("enter dishname or q to quit ")
+    if (dishname!="q"){
+
+        dishes.push(dishname)
+    }
+    else{
+        break
+    }
+    
+}
+console.log(dishes)*/
+
